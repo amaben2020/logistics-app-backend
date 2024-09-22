f9423a1e6030326b144e2aee968e5ff7ef54d7b6
Algomachine <ben@truckup.com>
2024-09-22 16:40:50 +0100

    Project setup

---
+++ packages/functions/src/todo.ts
@@ -0,0 +1,19 @@
+import { ApiHandler } from 'sst/node/api';
+
+import { Todo } from '@logistics-app/core/todo';
+
+export const create = ApiHandler(async (_evt) => {
+  await Todo.create();
+
+  return {
+    statusCode: 200,
+    body: 'Todo created',
+  };
+});
+
+export const list = ApiHandler(async (_evt) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(Todo.list()),
+  };
+});
